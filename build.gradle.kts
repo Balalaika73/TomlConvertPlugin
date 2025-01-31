2d2020b1945254fa8b16bad752d98d75e9e4b80e
Administrator <admin@example.com>
2025-01-31 21:58:32 +0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.9.23"
+    id("org.jetbrains.intellij") version "1.17.3"
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("241.19072.14.2412.12360217")
+    type.set("AI") // Target IDE Platform
+
+    plugins.set(listOf("Kotlin", "android"))
+}
+
+tasks {
+    buildSearchableOptions {
+        enabled = false
+    }
+    runIde {
+        // Absolute path to installed target 3.5 Android Studio to use as
+        // IDE Development Instance (the "Contents" directory is macOS specific):
+        ideDir.set(file("C:/Program Files/Android/Android Studio"))
+    }
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("241")
+        untilBuild.set("242.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
---
+++ src/main/kotlin/MyAction.kt
@@ -0,0 +1,188 @@
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiManager
+import java.io.File
+
+class MyAction: AnAction() {
+    private var fileToml : VirtualFile? = null
+    private var filePluginProject : VirtualFile? = null
+    private var filePluginModule : VirtualFile? = null
+    private lateinit var project: Project
+
+    override fun actionPerformed(e: AnActionEvent) {
+        project = e.project ?: return
+        fileToml = findFileInProject("gradle/libs.versions.toml")
+        filePluginProject = findFileInProject("build.gradle.kts")
+        filePluginModule = findFileInProject("app/build.gradle.kts")
+        if (filePluginProject != null && fileToml != null && filePluginModule != null) {
+            // Прочитать содержимое файла
+            checkFile(e, filePluginProject!!,
+                {it.contains("id(")},
+                { convertProjectPluginToToml(it)})
+            checkFile(e, filePluginModule!!,
+                {it.contains("implementation(\"")},
+                { convertModuleToToml(it)})
+        } else {
+            showError(e, "File not found")
+        }
+    }
+
+    private fun findFileInProject(fileName: String): VirtualFile? {
+        val basePath = project.basePath ?: return null
+        val targetFile = File(basePath, fileName)
+        return VfsUtil.findFileByIoFile(targetFile, true)
+    }
+
+    private fun convertModuleToToml(line: String) {
+        val implParts = line.split(':')
+        val groupImpl = implParts[0].split('"')[1]
+        val nameImpl = implParts[1]
+        val versImpl = implParts[2].dropLast(2)
+
+        var versName = groupImpl.split('.').last() + "Version"
+        val resString = "$nameImpl = {group = \"$groupImpl\", name = \"$nameImpl\", version.ref = \"$versName\" }"
+
+        versName = "$versName = \"$versImpl\""
+
+        val implId = nameImpl.replace('-', '.')
+
+        // Перенос в TOML и удаление из оригинального файла
+        writeModuleToToml(resString, versName, implId)
+        removeLine(filePluginModule, line)
+    }
+
+    private fun writePluginToToml(tomlEntry: String, versionLine: String, pluginId: String) {
+        val tomlDocument = fileToml?.let { FileDocumentManager.getInstance().getDocument(it) }
+        val pluginDocument = filePluginProject?.let { FileDocumentManager.getInstance().getDocument(it) }
+        val moduleDocument = filePluginModule?.let { FileDocumentManager.getInstance().getDocument(it) }
+
+        WriteCommandAction.runWriteCommandAction(project) {
+            tomlDocument!!.let { doc ->
+                val librariesIndex = doc.text.indexOf("[libraries]")
+                if (librariesIndex != -1) {
+                    doc.insertString(librariesIndex, "\n$versionLine\n")
+                    doc.insertString(doc.textLength, "\n$tomlEntry")
+                }
+            }
+
+            pluginDocument!!.let { doc ->
+                val pluginsBlockEnd = doc.text.lastIndexOf("}")
+                if (pluginsBlockEnd != -1) {
+                    val aliasLine = "\n\talias(libs.plugins.$pluginId) apply false"
+                    doc.insertString(pluginsBlockEnd - 1, aliasLine)
+                }
+            }
+
+            moduleDocument!!.let { doc ->
+                val pluginsBlockEnd = doc.text.indexOf("}")
+                if (pluginsBlockEnd != -1) {
+                    val aliasLine = "\n\talias(libs.plugins.$pluginId)"
+                    doc.insertString(pluginsBlockEnd - 1, aliasLine)
+                }
+            }
+        }
+    }
+
+    private fun writeModuleToToml(tomlEntry: String, versionLine: String, pluginId: String) {
+        val tomlDocument = fileToml?.let { FileDocumentManager.getInstance().getDocument(it) }
+        val moduleDocument = filePluginModule?.let { FileDocumentManager.getInstance().getDocument(it) }
+
+        WriteCommandAction.runWriteCommandAction(project) {
+            tomlDocument?.let { doc ->
+                val versionLineToAdd = "\n$versionLine\n"
+                val tomlEntryToAdd = "\n$tomlEntry"
+
+                val versIndex = doc.text.indexOf("[libraries]")
+                if (versIndex != -1 && !doc.text.contains(versionLine)) {
+                    doc.insertString(versIndex, versionLineToAdd)
+                }
+
+                val librariesIndex = doc.text.indexOf("[plugins]")
+                if (librariesIndex != -1 && !doc.text.contains(tomlEntry)) {
+                    Messages.showMessageDialog(
+                        project,
+                        "$tomlEntryToAdd",
+                        "Error",
+                        Messages.getInformationIcon())
+                    doc.insertString(librariesIndex-1, tomlEntryToAdd)
+                }
+            }
+
+
+            moduleDocument?.let { doc ->
+                val pluginsBlockEnd = doc.text.lastIndexOf("}")
+                if (pluginsBlockEnd != -1) {
+                    val aliasLine = "\n\timplementation(libs.$pluginId)"
+                    doc.insertString(pluginsBlockEnd - 1, aliasLine)
+                }
+            }
+        }
+    }
+
+    fun removeLine(file: VirtualFile?, lineToRemove: String) {
+        file?.let {
+            FileDocumentManager.getInstance().getDocument(it)
+        }?.let { doc ->
+            WriteCommandAction.runWriteCommandAction(project) {
+                val updatedText = doc.text.lines().filter { it != lineToRemove }.joinToString("\n")
+                doc.setText(updatedText)
+            }
+        }
+    }
+
+    fun checkFile(e: AnActionEvent, file: VirtualFile, filterCondition: (String) -> Boolean, transform: (String) -> Unit) {
+        val content = readFileContent(file)
+
+        if (!content.isNullOrEmpty()) {
+            content.lines()
+                .filter(filterCondition) // Фильтруем строки по переданному условию
+                .forEach(transform) // Выполняем переданное преобразование
+        } else {
+            showError(e, "File is empty or cannot be read.")
+        }
+    }
+
+    private fun convertProjectPluginToToml(line: String) {
+        val pluginRegex = """id\("([^"]+)""".toRegex()
+        val versionRegex = """version\s+"([^"]+)"""".toRegex()
+
+        var pluginId = pluginRegex.find(line)?.groups?.get(1)?.value ?: "Unknown"
+
+        val startDotIndex = pluginId.indexOf('.', pluginId.indexOf('.') + 2)
+        val lastDotIndex = pluginId.lastIndexOf('.')
+
+        // form plugin name b vers name
+        val pluginName = pluginId.substring(startDotIndex + 1).replace('.', '-')
+        val pluginVersionName = "${pluginId.substring(lastDotIndex + 1)}Version"
+        val tomlEntry = "$pluginName = { id = \"$pluginId\", version.ref = \"$pluginVersionName\" }"
+        pluginId = pluginId.substring(startDotIndex + 1)
+
+        // take version value
+        val versionValue = versionRegex.find(line)?.groups?.get(1)?.value
+        val versionLine = versionValue?.let { "$pluginVersionName = \"$it\"" }
+
+        if (versionLine != null) {
+            writePluginToToml(tomlEntry, versionLine, pluginId)
+            removeLine(filePluginProject, line)
+        }
+    }
+
+    private fun readFileContent(file: VirtualFile): String? {
+        val psiFile = PsiManager.getInstance(project).findFile(file)
+        return psiFile?.text
+    }
+
+    private fun showError(event: AnActionEvent, e: String){
+        Messages.showMessageDialog(
+            event.project,
+            "Error: $e",
+            "Error",
+            Messages.getInformationIcon());
+    }
+}
